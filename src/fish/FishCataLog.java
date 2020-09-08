0f9c20df162f730a18a89f21141937f4450e3ef9
aaron3231 <aaron11401@gmail.com>
2020-09-09 00:21:46 +0800

    TEST_fix_FishCataLog_encode

--- src/fish/FishCataLog.java
+++ src/fish/FishCataLog.java
@@ -17,31 +17,31 @@ public class FishCataLog {
 	private static final SecureRandom randomNumbers = new SecureRandom();
 	
 	
-	public enum EatType {MEAT,HERBIVOROUS,OMNIVORE};//���Y���F��(�׭��A�󭹡A����)
-	private static final int YEAR = 8760;//�@�~���X�p�ɡA�p���
+	public enum EatType {MEAT,HERBIVOROUS,OMNIVORE};//魚吃的東西(肉食，草食，雜食)
+	private static final int YEAR = 8760;//一年有幾小時，計算用
 	private static final int FishTypeNumber = 10;
 	String[] fishName=new String[FishTypeNumber];
-	double[] lifeEnd=new double[FishTypeNumber];//�ةR(�~)
-	double[] lifeEndRange=new double[FishTypeNumber];//�ةR�B��(�~)
-	int[] initLively=new int[FishTypeNumber];//�_�l�����
+	double[] lifeEnd=new double[FishTypeNumber];//壽命(年)
+	double[] lifeEndRange=new double[FishTypeNumber];//壽命浮動(年)
+	int[] initLively=new int[FishTypeNumber];//起始活潑度
 	double[] maxLength=new double[FishTypeNumber];
-	double[] maxLengthRange=new double[FishTypeNumber];//���ת��B�ʭ�
-	int[] initFight=new int[FishTypeNumber];//�_�l���[��
-	//��l���׬��̤j���ת�1/3;
-	//�魫����(����*10)g
-	//�̤j�����׬�(�魫)
-	//�_�l�����׬���
+	double[] maxLengthRange=new double[FishTypeNumber];//長度的浮動值
+	int[] initFight=new int[FishTypeNumber];//起始打架度
+	//初始長度為最大長度的1/3;
+	//體重等於(身長*10)g
+	//最大飽食度為(體重)
+	//起始飽食度為滿
 	EatType[] eating=new EatType[FishTypeNumber];
-	int[] suitableTemperature=new int[FishTypeNumber];//�A�X�ͦs�ūסA��abc0def���ab.c�ס�ed.f��
+	int[] suitableTemperature=new int[FishTypeNumber];//適合生存溫度，用abc0def表示ab.c度～ed.f度
 	
 	
-	String[] statement=new String[FishTypeNumber];	//²���A����A��
+	String[] statement=new String[FishTypeNumber];	//簡介，之後再補
 	
 	
 	public FishCataLog(){
-		//������
-		//�ŭ˦Q
-		fishName[0]="�ŭ˦Q";
+		//海水魚
+		//藍倒吊
+		fishName[0]="藍倒吊";
 		lifeEnd[0]=5;
 		lifeEndRange[0]=2;
 		initLively[0]=50;
@@ -51,8 +51,8 @@ public class FishCataLog {
 		suitableTemperature[0]=2400260;
 		initFight[0]=20;
 		
-		//���]�����u
-		fishName[1]="���]�����u";
+		//藍魔鬼砲彈
+		fishName[1]="藍魔鬼砲彈";
 		lifeEnd[1]=5;
 		lifeEndRange[1]=2;
 		initLively[1]=60;
@@ -62,8 +62,8 @@ public class FishCataLog {
 		suitableTemperature[1]=2400270;
 		initFight[1]=50;
 		
-		//���{�q
-		fishName[2]="���{�q";
+		//紅閃電
+		fishName[2]="紅閃電";
 		lifeEnd[2]=3;
 		lifeEndRange[2]=2;
 		initLively[2]=60;
@@ -73,8 +73,8 @@ public class FishCataLog {
 		suitableTemperature[2]=2400270;
 		initFight[2]=50;
 		
-		//���Y��
-		fishName[3]="���Y��";
+		//藍頭蝶
+		fishName[3]="藍頭蝶";
 		lifeEnd[3]=10;
 		lifeEndRange[3]=2;
 		initLively[3]=50;
@@ -84,8 +84,8 @@ public class FishCataLog {
 		suitableTemperature[3]=2450255;
 		initFight[3]=20;
 		
-		//���ԧB���P��
-		fishName[4]="���ԧB���P��";
+		//阿拉伯神仙魚
+		fishName[4]="阿拉伯神仙魚";
 		lifeEnd[4]=10;
 		lifeEndRange[4]=2;
 		initLively[4]=50;
@@ -96,10 +96,10 @@ public class FishCataLog {
 		initFight[4]=10;
 		
 		
-		//�H����
-		//�\������
-		fishName[5]="�\������";
-		lifeEnd[5]=2;	//�۩�2~3�~, ����3~6�~�A�]�ثe�S���ʧO�G�X�b�@�_
+		//淡水魚
+		//蓋斑鬥魚
+		fishName[5]="蓋斑鬥魚";
+		lifeEnd[5]=2;	//雌性2~3年, 雄性3~6年，因目前沒分性別故合在一起
 		lifeEndRange[5]=4;
 		initLively[5]=60;
 		maxLength[5]=5;
@@ -108,8 +108,8 @@ public class FishCataLog {
 		suitableTemperature[5]=2000270;
 		initFight[5]=60;
 		
-		//���갫��
-		fishName[6]="���갫��";
+		//泰國鬥魚
+		fishName[6]="泰國鬥魚";
 		lifeEnd[6]=1.5;
 		lifeEndRange[6]=0.5;
 		initLively[6]=60;
@@ -119,8 +119,8 @@ public class FishCataLog {
 		suitableTemperature[6]=2200240;
 		initFight[6]=50;
 		
-		//����O
-		fishName[7]="����O";
+		//日光燈
+		fishName[7]="日光燈";
 		lifeEnd[7]=1;
 		lifeEndRange[7]=1;
 		initLively[7]=40;
@@ -130,8 +130,8 @@ public class FishCataLog {
 		suitableTemperature[7]=2000260;
 		initFight[7]=10;
 		
-		//������
-		fishName[8]="������";
+		//斑馬魚
+		fishName[8]="斑馬魚";
 		lifeEnd[8]=2;
 		lifeEndRange[8]=1;
 		initLively[8]=45;
@@ -141,8 +141,8 @@ public class FishCataLog {
 		suitableTemperature[8]=2500260;
 		initFight[8]=10;
 		
-		//�ճ���
-		fishName[9]="�ճ���";
+		//孔雀魚
+		fishName[9]="孔雀魚";
 		lifeEnd[9]=1;
 		lifeEndRange[9]=1;
 		initLively[9]=55;
@@ -164,7 +164,7 @@ public class FishCataLog {
 		int fight;
 		switch (fishName)
 		{
-			case "�ŭ˦Q":
+			case "藍倒吊":
 				lifeEnd=(int)(this.lifeEnd[0]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[0]*10));
 				maxWeight=(int)(this.maxLength[0]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[0]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -173,7 +173,7 @@ public class FishCataLog {
 				temp=new ParacanthurusHepatus(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "���]�����u":
+			case "藍魔鬼砲彈":
 				lifeEnd=(int)(this.lifeEnd[1]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[1]*10));
 				maxWeight=(int)(this.maxLength[1]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[1]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -182,7 +182,7 @@ public class FishCataLog {
 				temp=new OdunusNiger(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "���{�q":
+			case "紅閃電":
 				lifeEnd=(int)(this.lifeEnd[2]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[2]*10));
 				maxWeight=(int)(this.maxLength[2]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[2]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -191,7 +191,7 @@ public class FishCataLog {
 				temp=new CentropygeFerrugata(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "���Y��":
+			case "藍頭蝶":
 				lifeEnd=(int)(this.lifeEnd[3]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[3]*10));
 				maxWeight=(int)(this.maxLength[3]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[3]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -200,7 +200,7 @@ public class FishCataLog {
 				temp=new ChaetodonKleinii(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "���ԧB���P��":
+			case "阿拉伯神仙魚":
 				lifeEnd=(int)(this.lifeEnd[4]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[4]*10));
 				maxWeight=(int)(this.maxLength[4]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[4]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -209,7 +209,7 @@ public class FishCataLog {
 				temp=new PomacanthusAsfur(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "�\������":
+			case "蓋斑鬥魚":
 				lifeEnd=(int)(this.lifeEnd[5]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[5]*10));
 				maxWeight=(int)(this.maxLength[5]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[5]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -218,7 +218,7 @@ public class FishCataLog {
 				temp=new MacropodusOpercularis(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "���갫��":
+			case "泰國鬥魚":
 				lifeEnd=(int)(this.lifeEnd[6]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[6]*10));
 				maxWeight=(int)(this.maxLength[6]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[6]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -227,7 +227,7 @@ public class FishCataLog {
 				temp=new BettaSplendens(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "����O":
+			case "日光燈":
 				lifeEnd=(int)(this.lifeEnd[7]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[7]*10));
 				maxWeight=(int)(this.maxLength[7]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[7]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -236,7 +236,7 @@ public class FishCataLog {
 				temp=new ParacheirodonInnesi(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "������":
+			case "斑馬魚":
 				lifeEnd=(int)(this.lifeEnd[8]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[8]*10));
 				maxWeight=(int)(this.maxLength[8]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[8]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -245,7 +245,7 @@ public class FishCataLog {
 				temp=new DarioRerio(weight,lively,lifeEnd,maxWeight,fight);
 				break;
 				
-			case "�ճ���":
+			case "孔雀魚":
 				lifeEnd=(int)(this.lifeEnd[9]*YEAR)+randomNumbers.nextInt((int)(this.lifeEndRange[9]*10));
 				maxWeight=(int)(this.maxLength[9]*10)+randomNumbers.nextInt((int)(this.maxLengthRange[9]*10));
 				weight=maxWeight/3+randomNumbers.nextInt(5)-2;
@@ -266,52 +266,52 @@ public class FishCataLog {
 		if(fish.getClass()==(new ParacanthurusHepatus(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish--;
-			return "�ŭ˦Q";
+			return "藍倒吊";
 		}
 		else if(fish.getClass()==(new OdunusNiger(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=2;
-			return "���]�����u";
+			return "藍魔鬼砲彈";
 		}
 		else if(fish.getClass()==(new CentropygeFerrugata(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=3;
-			return "���{�q";
+			return "紅閃電";
 		}
 		else if(fish.getClass()==(new ChaetodonKleinii(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=4;
-			return "���Y��";
+			return "藍頭蝶";
 		}
 		else if(fish.getClass()==(new PomacanthusAsfur(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=5;
-			return "���ԧB���P��";
+			return "阿拉伯神仙魚";
 		}
 		else if(fish.getClass()==(new MacropodusOpercularis(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=6;
-			return "�\������";
+			return "蓋斑鬥魚";
 		}
 		else if(fish.getClass()==(new BettaSplendens(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=7;
-			return "���갫��";
+			return "泰國鬥魚";
 		}
 		else if(fish.getClass()==(new ParacheirodonInnesi(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=8;
-			return "����O";
+			return "日光燈";
 		}
 		else if(fish.getClass()==(new DarioRerio(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=9;
-			return "������";
+			return "斑馬魚";
 		}
 		else if(fish.getClass()==(new PoeciliaReticulata(0,0,0,0,0).getClass()))
 		{
 			Fish.totalFish-=10;
-			return "�ճ���";
+			return "孔雀魚";
 		}
 		
 		return "zzz";
